9e40f8e1e9eded575b6f546ed3b603f012d1e1c5
LiXuFei <xfli@thoughtworks.com>
2016-09-06 09:26:26 +0800

    set capybara screen path.

--- calculator/features/support/env.rb
+++ calculator/features/support/env.rb
@@ -2,3 +2,4 @@ require File.join(File.dirname(__FILE__), 'nice_bank')
 require 'capybara/cucumber'
 Capybara.app = Sinatra::Application
 Sinatra::Application.set :environment, :test
+Capybara.save_and_open_page_path = Dir.pwd+"/features/reports"
